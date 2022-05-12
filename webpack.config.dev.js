079f16169cdb2282a8a595886a4356b1e225565e
LuoHao <hluo5206@gmail.com>
2022-05-12 23:26:39 +0800

    feat(historyapifallback): historyApiFallback
    historyApiFallback

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,9 +2,21 @@ const config = require("./webpack.config.js");
 const { merge } = require("webpack-merge");
 module.exports = merge(config, {
   mode: "development",
-  devtool: 'source-map',
+  devtool: "source-map",
   watch: true,
-  // devServer: {
-  //   port: 9000,
-  // },
+  devServer: {
+    port: 8080,
+    historyApiFallback: {
+      rewrites: [
+        {
+          from: /\/index.html/,
+          to: "/index.html",
+        },
+        {
+          from: /\/mobile.html/,
+          to: "/mobile.html",
+        },
+      ],
+    },
+  },
 });
